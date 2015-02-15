46f15a96cb8e33149b15dec5ee4229ae7d3b2e61
David C. Drake <dcdrake42@gmail.com>
2015-02-14 18:26:58 -0700

    Add "string_manipulation.rb"

---
+++ string_modification.rb
@@ -0,0 +1,9 @@
+print "Input: "
+user_input = gets.chomp
+if user_input.include? 's'
+  user_input.gsub!(/s/, 'th')
+  user_input.gsub!(/S/, 'Th')
+else
+  puts "No \"s\" found in input string."
+end
+puts "Output: #{user_input}"
