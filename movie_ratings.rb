e400e502e5791e6b1c4f4ec3599dc3fdc87b8ccb
David C. Drake <dcdrake42@gmail.com>
2015-02-15 19:43:58 -0700

    Add "movie_ratings.rb"

---
+++ movie_ratings.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/ruby
+
+movies = {
+  "Nausicaa of the Valley of the Wind": 5,
+  "My Neighbor Totoro": 5,
+  "Kiki's Delivery Service": 5,
+  "Porco Rosso": 5,
+}
+
+puts movies
+
+begin
+  puts "What do you want to do (display, add, update, delete, or quit)? "
+  choice = gets.chomp.downcase
+  case choice
+    when "add"
+      puts "Title: "
+      title = gets.chomp.to_sym
+      puts "Rating: "
+      rating = gets.chomp.to_i
+      if movies[title].nil?
+        movies[title] = rating
+        puts "#{title} added!"
+      else
+        puts "#{title} already exists."
+      end
+    when "update"
+      puts "Title: "
+      title = gets.chomp.to_sym
+      if movies[title].nil?
+        puts "#{title} not found."
+      else
+        puts "New rating: "
+        rating = gets.chomp.to_i
+        movies[title] = rating
+        puts "#{title} updated."
+      end
+    when "display"
+      movies.each do |title, rating|
+        puts "#{title}: #{rating}"
+      end
+    when "delete"
+      puts "Title: "
+      title = gets.chomp.to_sym
+      if movies[title].nil?
+        puts "#{title} not found."
+      else
+        movies.delete(title)
+        puts "#{title} deleted."
+      end
+    else
+      puts "Input not recognized. Please try again!"
+  end
+end until choice == "quit"
+
+puts movies
